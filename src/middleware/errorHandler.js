22db991d6ef08a36a804833c0468b4ecf6961766
lilfortunaa <bogdanchikzaichik@gmail.com>
2025-10-05 11:22:26 +0200

    errorHandler.js

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -5,16 +5,11 @@ export const errorHandler = (err, req, res, next) => {
 
   if (err instanceof HttpError) {
     return res.status(err.status).json({
-    error: err.message || err.name,
+      message: err.message || err.name,
+    });
+  }
+
+  res.status(500).json({
+    message: err.message || "Something went wrong. Please try again later.",
   });
- }
-
- const isProd = process.env.NODE_ENV === "production";
-
-
- res.status(500).json({
-  message: isProd
-    ? "Something went wrong. Please try again later."
-    : err.message,
- });
 };
