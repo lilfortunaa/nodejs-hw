7f36478a4b855efdcb29da716504adb1730bf4cb
lilfortunaa <bogdanchikzaichik@gmail.com>
2025-10-10 10:34:36 +0200

    03-validation

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -23,7 +23,7 @@ export const noteIdSchema = {
 };
 
 export const createNoteSchema = {
-  [Segments.QUERY]: Joi.object({
+  [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
     content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
